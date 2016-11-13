b0f7b2a70511014ee5b3e0666d0e89d05c714a83
Daniel <minsoung@ualberta.ca>
2016-11-12 17:50:46 -0700

    changed margin

--- HeatMap.js
+++ HeatMap.js
@@ -1,9 +1,9 @@
 var width = 1000;
-var height = 1100;
+var height = 950;
 
 var projection = d3.geoMercator()
 					.scale(100000)
-					.center([-113.48, 53.533])
+					.center([-113.48, 53.557])
 					.translate([width/2, height/2]);
 
 var path = d3.geoPath()
@@ -12,9 +12,7 @@ var path = d3.geoPath()
 var svg = d3.select("div")
 			.append("svg")
 			.attr("width", width)
-			.attr("height", height)
-			.attr("stroke", "#aaa")
-			.attr("fill", "pink");
+			.attr("height", height);
 
 var g = svg.append("g");
 
@@ -24,7 +22,10 @@ d3.json("data/yeg.geojson", function(mapData) {
 	g.selectAll("path")
 		.data(features)
 		.enter().append("path")
-		.attr("d", path);
+		.attr("d", path)
+		.attr("fill", "pink")
+		.attr("stroke", "#aaa");
 
+	console.log(mapData);
 	console.log(features);
 });
