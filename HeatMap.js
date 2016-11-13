1a6bd27f67d065cb37069b83e0fd793f68d219b4
Abid Rahman <arahman1@ualberta.ca>
2016-11-13 11:20:07 -0700

    Added dem pics to make us look good

--- HeatMap.js
+++ HeatMap.js
@@ -36,13 +36,62 @@ d3.select("svg").append("text")
 d3.select("svg").append("text")
 	.attr("id", "text1")
 	.attr("x", 5)
-	.attr("y", textHeight);
+	.attr("y", textHeight)
+  .style("font-size", "30px");
 
 d3.select("svg").append("text")
 	.attr("id", "text2")
 	.attr("x", 5)
-	.attr("y", textHeight + 25)
-	.style("font-size", "20px");
+	.attr("y", textHeight + 30)
+	.style("font-size", "25px");
+
+d3.select("svg").append("text")
+  .attr("id", "text3")
+  .attr("x", 5)
+  .attr("y", textHeight + 60)
+  .style("font-size", "20px");
+
+d3.select("svg").append("text")
+  .attr("id", "text4")
+  .attr("x", 5)
+  .attr("y", textHeight + 80)
+  .style("font-size", "20px");
+
+d3.select("svg").append("text")
+  .attr("id", "text5")
+  .attr("x", 5)
+  .attr("y", textHeight + 100)
+  .style("font-size", "20px");
+
+d3.select("svg").append("text")
+  .attr("id", "text6")
+  .attr("x", 5)
+  .attr("y", textHeight + 120)
+  .style("font-size", "20px");
+
+d3.select("svg").append("text")
+  .attr("id", "text7")
+  .attr("x", 5)
+  .attr("y", textHeight + 140)
+  .style("font-size", "20px");
+
+d3.select("svg").append("text")
+  .attr("id", "text8")
+  .attr("x", 5)
+  .attr("y", textHeight + 160)
+  .style("font-size", "20px");
+
+d3.select("svg").append("text")
+  .attr("id", "text9")
+  .attr("x", 5)
+  .attr("y", textHeight + 180)
+  .style("font-size", "20px");
+
+d3.select("svg").append("text")
+  .attr("id", "text10")
+  .attr("x", 5)
+  .attr("y", textHeight + 200)
+  .style("font-size", "20px");
 
 var g = svg.append("g");
 
@@ -50,14 +99,26 @@ var g = svg.append("g");
 d3.csv("data/data.csv", function(crimeData) {
 	for (i = 0; i < crimeData.length; i++) {
       if (!(crimeData[i].neighbourhood in totalCrimeData)) {
-        totalCrimeData[crimeData[i].neighbourhood] = parseInt(crimeData[i].no_of_crime);
-        
+        totalCrimeData[crimeData[i].neighbourhood] = {"Total" : 0,
+                                                      "Assault": 0, 
+                                                      "Break and Enter": 0,
+                                                      "Homicide": 0,
+                                                      "Robbery": 0,
+                                                      "Sexual Assaults": 0,
+                                                      "Theft From Vehicle": 0,
+                                                      "Theft Of Vehicle": 0,
+                                                      "Theft Over $5000": 0
+                                                      };
+        totalCrimeData[crimeData[i].neighbourhood]["Total"] = parseInt(crimeData[i].no_of_crime);
+        totalCrimeData[crimeData[i].neighbourhood][crimeData[i].type_crime] += parseInt(crimeData[i].no_of_crime);
       }
       else {
-        totalCrimeData[crimeData[i].neighbourhood] += parseInt(crimeData[i].no_of_crime);	
+        totalCrimeData[crimeData[i].neighbourhood]["Total"] += parseInt(crimeData[i].no_of_crime);
+        totalCrimeData[crimeData[i].neighbourhood][crimeData[i].type_crime] += parseInt(crimeData[i].no_of_crime);
       }
+    
 	}
-	var arr = Object.keys(totalCrimeData).map(function(key) {return totalCrimeData[key];});
+	var arr = Object.keys(totalCrimeData).map(function(key) {return totalCrimeData[key]["Total"];});
 	var min = Math.min.apply(null, arr);
 	var max = Math.max.apply(null, arr);
 
@@ -69,13 +130,17 @@ d3.csv("data/data.csv", function(crimeData) {
 					.range([0, 0.9]);
 
 		var colourmap = function(d) {
-							if (totalCrimeData[d.properties.name.toUpperCase()] == undefined) {
+            if (d.properties.name.toUpperCase() in totalCrimeData) {
+							if (totalCrimeData[d.properties.name.toUpperCase()]["Total"] == undefined) {
 								return 0;
 							}
 							else {
-								return scale(totalCrimeData[d.properties.name.toUpperCase()]);
+								return scale(totalCrimeData[d.properties.name.toUpperCase()]["Total"]);
 							}
-						};
+						} else {
+              return 0;
+            }
+    };
 
 		g.selectAll("path")
 			.data(features)
@@ -92,14 +157,33 @@ d3.csv("data/data.csv", function(crimeData) {
 					.text(d3.select(this).data()[0].properties.name);
 				d3.select("#text2")
 					.text("Number of Crimes: " + 
-						totalCrimeData[d3.select(this).data()[0].properties.name.toUpperCase()]);
+						totalCrimeData[d3.select(this).data()[0].properties.name.toUpperCase()]["Total"]);
 			})
+      .on("mousedown", function() {
+        d3.select("#text3").text("Assault: " + totalCrimeData[d3.select(this).data()[0].properties.name.toUpperCase()]["Assault"])
+        d3.select("#text4").text("Break and Enter: " + totalCrimeData[d3.select(this).data()[0].properties.name.toUpperCase()]["Break and Enter"])
+        d3.select("#text5").text("Homicide: " + totalCrimeData[d3.select(this).data()[0].properties.name.toUpperCase()]["Homicide"])
+        d3.select("#text6").text("Robbery: " + totalCrimeData[d3.select(this).data()[0].properties.name.toUpperCase()]["Robbery"])
+        d3.select("#text7").text("Sexual Assaults: " + totalCrimeData[d3.select(this).data()[0].properties.name.toUpperCase()]["Sexual Assaults"])
+        d3.select("#text8").text("Theft From Vehicle: " + totalCrimeData[d3.select(this).data()[0].properties.name.toUpperCase()]["Theft From Vehicle"])
+        d3.select("#text9").text("Theft Of Vehicle: " + totalCrimeData[d3.select(this).data()[0].properties.name.toUpperCase()]["Theft Of Vehicle"])
+        d3.select("#text10").text("Theft Over $5000: " + totalCrimeData[d3.select(this).data()[0].properties.name.toUpperCase()]["Theft Over $5000"])
+      })
 			.on("mouseout", function() {
 				d3.select(this)
 				.style("fill", "rgb(204, 0, 0)")
 				.style("opacity", colourmap);
 				d3.select("#text1").text("");
 				d3.select("#text2").text("");
+        d3.select("#text3").text("");
+        d3.select("#text4").text("");
+        d3.select("#text5").text("");
+        d3.select("#text6").text("");
+        d3.select("#text7").text("");
+        d3.select("#text8").text("");
+        d3.select("#text9").text("");
+        d3.select("#text10").text("");
+      
 			});
 	});
 });
